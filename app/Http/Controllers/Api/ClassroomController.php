b8137138c93f9f2472340c456514986109a6b8c2
Brayan Mota <brayannogueira@unitins.br>
2021-11-21 16:02:25 -0300

    Atualizando

--- app/Http/Controllers/Api/ClassroomController.php
+++ app/Http/Controllers/Api/ClassroomController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Classroom;
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Api/SemesterController.php
+++ app/Http/Controllers/Api/SemesterController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Campus;
 use App\Models\Semester;
 use App\Repositories\Eloquent\BaseRepository;
